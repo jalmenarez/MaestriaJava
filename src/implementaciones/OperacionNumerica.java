37ecad904d3421d9373d0a7aa5019e889bb313ba
Jose Almenarez <jose.almenarez@gmail.com>
2013-04-26 20:44:09 -0400

    Clase que implementa la interfaz IEstadisticas.

---
+++ src/implementaciones/OperacionNumerica.java
@@ -0,0 +1,35 @@
+package implementaciones;
+
+import interfaces.IEstadisticas;
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * 
+ * @author jalmenarez
+ */
+public class OperacionNumerica implements IEstadisticas {
+    
+    @Getter @Setter //Se usa el Project Lombok
+    private double[] valoresNumericos;
+
+    public OperacionNumerica(int cant) {
+        this.valoresNumericos = new double[cant];
+    }
+    
+    public void asignaValoresDouble(){
+        for(int i=0;i<this.valoresNumericos.length;i++){
+            this.valoresNumericos[i]=Math.random();
+        }
+    }
+    
+    public double valorMinimo() {
+        double menorValor = this.valoresNumericos[0];
+        for (int i = 0; i < this.valoresNumericos.length; i++) {
+            if(menorValor>this.valoresNumericos[i])
+                menorValor = this.valoresNumericos[i];
+        }
+        return menorValor;
+    }
+
+}
