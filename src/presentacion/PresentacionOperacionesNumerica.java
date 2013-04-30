84f2db44a81a8f90effe306a505d54d4f16bdfc8
Jose Almenarez <jose.almenarez@gmail.com>
2013-04-30 18:43:57 -0400

    Se agrega el manejo de excepciones.

--- src/presentacion/PresentacionOperacionesNumerica.java
+++ src/presentacion/PresentacionOperacionesNumerica.java
@@ -1,6 +1,8 @@
 
 package presentacion;
 
+import excepciones.ExcepcionLargoMinimoArray;
+import excepciones.ExcepcionValorMinimo;
 import implementaciones.OperacionesNumerica;
 
 /**
@@ -13,12 +15,16 @@ public class PresentacionOperacionesNumerica {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        double[] arregloDobles = new double[10];
+        double[] arregloDobles = new double[5];
         OperacionesNumerica oOperNumerica = new OperacionesNumerica();
-        
+        try {
         oOperNumerica.setValoresNumericos(arregloDobles);
         oOperNumerica.asignaValoresDouble();
-        
         System.out.println("El valor minimo encontrado en el array de double es: "+ oOperNumerica.valorMinimo());
+        }catch(ExcepcionLargoMinimoArray e){
+        System.out.println(e.getMessage());
+        } catch (ExcepcionValorMinimo e) {
+        System.out.println(e.getMessage());
+        }    
     }
 }
