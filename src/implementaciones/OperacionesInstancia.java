97687a3835612358790c1554564db1c7324d1f90
Jose Almenarez <jose.almenarez@gmail.com>
2013-04-30 19:06:39 -0400

    Se modifica la clase para que maneje excepciones.

--- src/implementaciones/OperacionesInstancia.java
+++ src/implementaciones/OperacionesInstancia.java
@@ -1,14 +1,14 @@
 package implementaciones;
 
 import entidades.Accion;
+import excepciones.ExcepcionLargoMinimoArray;
+import excepciones.ExcepcionValorMinimo;
 import interfaces.IEstadisticas;
 import lombok.Getter;
-import lombok.Setter;
 
 public class OperacionesInstancia implements IEstadisticas {
     
     @Getter 
-    @Setter
     private Accion[] valoresInstancia;
     
     public OperacionesInstancia(int cant) {
@@ -18,6 +18,13 @@ public class OperacionesInstancia implements IEstadisticas {
     public OperacionesInstancia() {
         
     }
+
+    public void setValoresInstancia(Accion[] valoresInstancia) throws ExcepcionLargoMinimoArray {
+        if (valoresInstancia.length<5)
+            throw new ExcepcionLargoMinimoArray("Accion",5);
+        
+        this.valoresInstancia = valoresInstancia;
+    }
     
     public void asignaValoresInstancia(){
         for (int i = 0; i < this.valoresInstancia.length; i++) {
@@ -25,12 +32,16 @@ public class OperacionesInstancia implements IEstadisticas {
         }
     }
     
-    public double valorMinimo() {
+    public double valorMinimo() throws ExcepcionValorMinimo {
         double menorValor = this.valoresInstancia[0].getValorAccion();
         for (int i = 0; i < this.valoresInstancia.length; i++) {
             if(menorValor>this.valoresInstancia[i].getValorAccion())
                 menorValor = this.valoresInstancia[i].getValorAccion();
         }
+        
+        if(menorValor<0.5)
+            throw new ExcepcionValorMinimo("0.5");
+        
         return menorValor;
     }
 
