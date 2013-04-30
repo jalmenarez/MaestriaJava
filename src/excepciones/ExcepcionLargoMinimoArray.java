679b079a9cbffc0a209c18d1aaebb8a8dfc887ce
Jose Almenarez <jose.almenarez@gmail.com>
2013-04-30 18:47:35 -0400

    Se modifica el mensaje de la excepcion.

--- src/excepciones/ExcepcionLargoMinimoArray.java
+++ src/excepciones/ExcepcionLargoMinimoArray.java
@@ -2,6 +2,6 @@ package excepciones;
 
 public class ExcepcionLargoMinimoArray extends Exception {
     public ExcepcionLargoMinimoArray(String nombreClase){
-        super("El array de "+nombreClase+" no debe ser menor a 5 elementos de largo.");
+        super("El array de tipo "+nombreClase+" no debe ser menor a 5 elementos de largo.");
     }
 }
