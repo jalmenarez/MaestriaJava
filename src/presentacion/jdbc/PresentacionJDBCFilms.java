b4a3cb8226a3468567a323ae565a0174754da026
Jose Almenarez <jose.almenarez@gmail.com>
2013-05-06 20:27:27 -0400

    Se implementa el metodo para obtener un Film como array de Strings.

--- src/presentacion/jdbc/PresentacionJDBCFilms.java
+++ src/presentacion/jdbc/PresentacionJDBCFilms.java
@@ -8,12 +8,15 @@ public class PresentacionJDBCFilms {
         ServicioJDBCFilms oSerJDBC=new ServicioJDBCFilms();
         //Conecta a la base de datos
         oSerJDBC.conectaBD();
-        if(oSerJDBC.agregarNuevoFilm(1, "La Odisea", "Prometeo")){
+        /*if(oSerJDBC.agregarNuevoFilm(2, "La Odisea 2", "Prometeo 2")){
          System.out.println("Se agregó el registro!");
         }else{
          System.out.println("No se agregó el registro!");   
-        }
-        
+        }*/
+        String[] film = oSerJDBC.getFilmByID(1);
+        System.out.println("Film: "+film[0]);
+        System.out.println("Film Name: "+film[1]);
+        System.out.println("Film Autor: "+film[2]);      
         //Desconecta de la bade de datos
         oSerJDBC.desconectaBD();
     }
